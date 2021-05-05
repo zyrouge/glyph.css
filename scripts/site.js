05a9c1c8a6c9d16525823ff7d0c04ea2e65e5629
ZYROUGE <zyrouge@hotmail.com>
2021-05-05 21:35:47 +0530

    Fixed site

--- scripts/site.js
+++ scripts/site.js
@@ -1,13 +1,32 @@
 const path = require("path");
 const fs = require("fs").promises;
+const util = require("util");
+const ncpCb = require("ncp");
 const glyph = require("../dist/generate").default;
 const { homepage } = require("../package.json");
 
 const DocsPath = path.resolve(__dirname, "..", "docs");
-const CnamePath = path.join(DocsPath, "CNAMEE");
+const CnamePath = path.join(DocsPath, "CNAME");
 const CssPath = path.join(DocsPath, "glyph.css");
+const SitePath = path.resolve(__dirname, "site");
+
+const ncp = util.promisify(ncpCb);
 
 const start = async () => {
+    await fs.rm(DocsPath, {
+        recursive: true,
+        force: true
+    });
+    console.log(`Removed: ${DocsPath}`);
+
+    await fs.mkdir(DocsPath, {
+        recursive: true
+    });
+    console.log(`Created: ${DocsPath}`);
+
+    await ncp(SitePath, DocsPath);
+    console.log(`Copied: ${SitePath} -> ${DocsPath}`);
+
     const styles = await glyph({
         prefix: ".demo",
     });
